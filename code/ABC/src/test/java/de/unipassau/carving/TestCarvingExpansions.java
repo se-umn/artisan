9d4cfa87170bc49059f38aa1200f0686e3da97a4
Alessio Gambi <gambi@st.cs.uni-saarland.de>
2018-03-10 07:11:44 +0100

    Passing test for simple static

--- code/ABC/src/test/java/de/unipassau/carving/TestCarvingExpansions.java
+++ code/ABC/src/test/java/de/unipassau/carving/TestCarvingExpansions.java
@@ -1,33 +1,45 @@
 package de.unipassau.carving;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Collection;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import de.unipassau.carving.carvers.Level_0_MethodCarver;
 import de.unipassau.data.Pair;
 import de.unipassau.data.Triplette;
+import de.unipassau.generation.TestCaseFactory;
+import de.unipassau.generation.TestGenerator;
+import soot.SootClass;
 
 public class TestCarvingExpansions {
+
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
 	// This method is commonly invoked as last in any system test on the created
 	// DummyInstance
 	private final String staticMethodToCarve = "<de.unipassau.testsubject.DummyObject: void end()>";
 
 	@Test
-	public void testExpansionWithSimpleStatic() throws FileNotFoundException, IOException {
+	public void testExpansionWithSimpleStatic() throws FileNotFoundException, IOException, InterruptedException {
 		File traceFile = new File("./src/test/resources/DummySystemTestGetSimple-trace.txt");
 		StackImplementation stackImplementation = new StackImplementation();
 		// Parsing
 		Triplette<ExecutionFlowGraph, DataDependencyGraph, CallGraph> parsedTrace = stackImplementation
 				.parseTrace(traceFile.getAbsolutePath());
 
-		visualize(parsedTrace);
+		// visualize(parsedTrace);
 
 		// Carving
 		Level_0_MethodCarver testCarver = new Level_0_MethodCarver(parsedTrace.getFirst(), parsedTrace.getSecond(),
@@ -35,32 +47,50 @@ public class TestCarvingExpansions {
 		List<Pair<ExecutionFlowGraph, DataDependencyGraph>> carvedTests = testCarver.carve(staticMethodToCarve);
 
 		// DEBUG MOSTLY
-		visualize(carvedTests);
+		// visualize(carvedTests);
 		/*
 		 * We expect 2 carved test for this method because 1 is the expanded and
 		 * 1 is with the method which returns the parameter
 		 */
 		assertEquals(2, carvedTests.size());
 
-		// // System tests contains 2 executions
-		//
-		// for (Pair<ExecutionFlowGraph, DataDependencyGraph> carvedTest :
-		// carvedTests) {
-		// // The only invocation in the test is the static invocation of the
-		// // method
-		// assertEquals(1,
-		// carvedTest.getFirst().getOrderedMethodInvocations().size());
-		// }
-		//
-		// String employeeProjectJar = "./src/test/resources/Employee.jar";
-		// TestGenerator testGenerator = new TestGenerator(employeeProjectJar);
-		// Collection<SootClass> testCases =
-		// testGenerator.generateTestCases(carvedTests);
-		// assertEquals(1, testCases.size());
-		//
+		String testSubjectJar = "./libs/testsubject-tests.jar";
+		TestGenerator testGenerator = new TestGenerator(testSubjectJar);
+		Collection<SootClass> testCases = testGenerator.generateTestCases(carvedTests);
+		assertEquals(1, testCases.size());
+
+		// Visual debug
 		// for (SootClass testCase : testCases) {
 		// JimpleUtils.prettyPrint(testCase);
 		// }
+
+		// Generate code
+		File outputDir = temporaryFolder.newFolder();
+		TestCaseFactory.generateTestFiles(outputDir, testCases);
+		
+		// TODO Assertion: 1 .class, 1 .java
+		assertEquals( 2, outputDir.listFiles().length);
+		
+		printJavaClasses( outputDir );
+
+	}
+
+	private final FilenameFilter javaFileNameFilter = new FilenameFilter() {
+		
+		@Override
+		public boolean accept(File dir, String name) {
+			return name.endsWith(".java");
+		}
+	};
+	
+	private void printJavaClasses(File outputDir) throws IOException {
+		for( File javaFile : outputDir.listFiles( javaFileNameFilter )){
+			System.out.println("File: " + javaFile.getAbsolutePath() );
+			for( String line : Files.readAllLines( javaFile.toPath(), Charset.defaultCharset())){
+				System.out.println( line );
+			}
+		}
+		
 	}
 
 	private void visualize(Triplette<ExecutionFlowGraph, DataDependencyGraph, CallGraph> parsedTrace) {
