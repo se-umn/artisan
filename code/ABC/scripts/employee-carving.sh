4a4191ae9c874fb6a345f6333de9a9bc81937024
Alessio Gambi <gambi@st.cs.uni-saarland.de>
2018-04-10 10:46:46 +0200

    Fixed script to use different carving methods

--- code/ABC/scripts/employee-carving.sh
+++ code/ABC/scripts/employee-carving.sh
@@ -14,7 +14,9 @@ PROJECT_JAR="../../../test-subjects/Examples/Employee/target/Employee-0.0.1-SNAP
 
 # Inputs
 TRACE_FILE="./employee-tracingOut/trace.txt"
+
 CARVE_BY="package=org.employee"
+
 EXTERNAL_INTERFACES="java.io.File java.nio.Path java.util.Scanner java.nio.file.Files"
 EXCLUDE_BY="package=org.employee.systemtest class=org.employee.Employee"
 TEST_SETUP_BY="class=org.employee.systemtest.SystemTestUtils"
@@ -22,8 +24,9 @@ TEST_SETUP_BY="class=org.employee.systemtest.SystemTestUtils"
 # Additional options for the carve command - mostly random choice for CG -verbose:gc
 export JAVA_OPTS="-Xmx4g -Xms512m -XX:+UseParallelGC -XX:NewRatio=2 -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO"
 
+set -x
 ${BIN_FOLDER}/carve \
-            --carve-by ${CARVE_BY} \
+            --carve-by "${CARVE_BY}" \
                 --trace-file ${TRACE_FILE} \
                 --project-jar ${PROJECT_JAR} \
                 --external ${EXTERNAL_INTERFACES} \
