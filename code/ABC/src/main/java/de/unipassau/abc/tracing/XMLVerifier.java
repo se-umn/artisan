91cad446d34d52ab66975fbf4579e385d051e1b0
Alessio Gambi <gambi@st.cs.uni-saarland.de>
2018-04-24 12:45:06 +0200

    Fix problem in XML validation

--- code/ABC/src/main/java/de/unipassau/abc/tracing/XMLVerifier.java
+++ code/ABC/src/main/java/de/unipassau/abc/tracing/XMLVerifier.java
@@ -21,39 +21,56 @@ public class XMLVerifier {
 	public static void verify(Object object, String xmlExpected) throws IOException {
 		// clear out existing permissions and set own ones
 		XStream xstream = new XStream();
-		
+
 		String expected = new String(Files.readAllBytes(Paths.get(xmlExpected)));
 		String actual = xstream.toXML(object);
 		//
-		org.junit.Assert.assertEquals("Object " + object + " does not match its serialized form " + XMLDumper.loadObject( xmlExpected ) + " inside file " + xmlExpected,
-				expected, actual);
+		org.junit.Assert.assertEquals("Object " + object + " does not match its serialized form "
+				+ XMLDumper.loadObject(xmlExpected) + " inside file " + xmlExpected, expected, actual);
 	}
 
-	// org.junit.ComparisonFailure: Object true does not match its expected value 1 expected:<[true]> but was:<[1]>
+	// org.junit.ComparisonFailure: Object true does not match its expected
+	// value 1 expected:<[true]> but was:<[1]>
 
 	// Primitive Values Are Different !
+	// Floating point numbers are store somehow with F and sometimes without it
+	// org.junit.ComparisonFailure: Object 69.0 does not match its expected
+	// value 69.0F expected:<69.0[F]> but was:<69.0[]>
+	// Called by Carver
 	public static void verifyPrimitive(Object boxedPrimitive, String expecteValueToString) throws IOException {
+
+		Object boxedExpectedValue = null;
 		
-		String actualValueToString = boxedPrimitive.toString();
-		
-		// org.junit.ComparisonFailure: Object true does not match its expected value 1 expected:<[true]> but was:<[1]>
-		if( "true".equalsIgnoreCase( actualValueToString) ){
-			actualValueToString = "1";
-		} else if ("false".equalsIgnoreCase( actualValueToString ) ){
-			actualValueToString = "0";
+		if (boxedPrimitive instanceof Boolean) {
+			if( "1".equals(expecteValueToString) ){
+				boxedExpectedValue  = Boolean.TRUE; 
+			} else if( "0".equals(expecteValueToString) ){
+				boxedExpectedValue  = Boolean.TRUE; 
+			}  else {
+				org.junit.Assert.fail("Unexpected value for boolean!");
+			}
+		} else if (boxedPrimitive instanceof Byte) {
+			// Not sure this is ok !
+			boxedExpectedValue = Byte.parseByte(expecteValueToString);
+		} else if (boxedPrimitive instanceof Character) {
+			boxedExpectedValue = Character.valueOf(expecteValueToString.charAt(0));
+		} else if (boxedPrimitive instanceof Double) {
+			boxedExpectedValue = Double.parseDouble( expecteValueToString );
+		} else if (boxedPrimitive instanceof Float) {
+			boxedExpectedValue  = Float.parseFloat( expecteValueToString );
+		} else if (boxedPrimitive instanceof Integer) {
+			boxedExpectedValue = Integer.parseInt( expecteValueToString );
+		} else if (boxedPrimitive instanceof Long) {
+			boxedExpectedValue = Long.parseLong( expecteValueToString );
+		} else if (boxedPrimitive instanceof Short) {
+			boxedExpectedValue = Short.parseShort( expecteValueToString );
+		} else {
+			org.junit.Assert.fail("Unexpected primitive type " + boxedPrimitive );
 		}
 		
-		if( "true".equalsIgnoreCase( expecteValueToString) ){
-			expecteValueToString = "1";
-		} else if ("false".equalsIgnoreCase( expecteValueToString ) ){
-			expecteValueToString = "0";
-		}
-		
-		
-		
-		// clear out existing permissions and set own ones
-		org.junit.Assert.assertEquals("Object " + actualValueToString + " does not match its expected value " + expecteValueToString,
-				expecteValueToString, actualValueToString);
+		org.junit.Assert.assertEquals(
+				"Object " + boxedPrimitive + " does not match its expected string value " + expecteValueToString,
+				boxedExpectedValue, boxedPrimitive);
 	}
 
 }
