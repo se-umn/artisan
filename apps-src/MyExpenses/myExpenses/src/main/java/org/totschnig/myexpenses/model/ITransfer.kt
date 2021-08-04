1e0b6bde327a769b7dae193737d1476b700f3ce1
Alessio Gambi <alessiogambi@gmail.com>
2021-08-04 11:03:00 +0200

    Cleaned up the local list of apps. We should switch to use submodules and the apps forked by Tymo instead

--- apps-src/MyExpenses/myExpenses/src/main/java/org/totschnig/myexpenses/model/ITransfer.kt
+++
@@ -1,7 +0,0 @@
-package org.totschnig.myexpenses.model
-
-interface ITransfer: ITransaction {
-    var transferPeer: Long?
-    val transferAccountId: Long?
-    var transferAmount: Money?
-}
--- apps-src/MyExpenses/myExpenses/src/main/java/org/totschnig/myexpenses/util/NougatFileProviderException.java
+++
@@ -1,7 +0,0 @@
-package org.totschnig.myexpenses.util;
-
-public class NougatFileProviderException extends IllegalStateException {
-  NougatFileProviderException(Throwable cause) {
-    super("On Nougat, falling back to file uri won't work", cause);
-  }
-}
