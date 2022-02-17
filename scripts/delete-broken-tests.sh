6473fdff2c66117779b60466a7df9624836a50df
Alessio Gambi <alessiogambi@gmail.com>
2022-02-17 17:29:02 +0100

    Tag broken and failed tests

--- scripts/delete-broken-tests.sh
+++ scripts/delete-broken-tests.sh
@@ -2,7 +2,7 @@
 while read -r TEST_NAME; do
     for TEST_FILE  in $(find  . -iname "${TEST_NAME}.java"); do
       echo "Deleting Broken Test ${TEST_FILE}" 
-      mv ${TEST_FILE} ${TEST_FILE}.deleted
+      mv ${TEST_FILE} ${TEST_FILE}.broken
     done
 done< <(cat carved-tests.log | grep "error: " | tr "." " " | awk '{print $1}' | tr "/" " " | awk '{print $NF}' | sort | uniq)
 # Report the count
