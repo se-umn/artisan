934dfbf13ec9b427586437977936ee6a63686ee8
Vsevolod Tymofyeyev <timvseffolod@gmail.com>
2022-03-03 03:12:19 +0100

    Update makefile generator

--- scripts/make-makefile.py
+++ scripts/make-makefile.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 from string import Template
 import configparser
@@ -10,9 +11,40 @@ template_path = os.path.join(scripts_dir, "make-template.make")
 def parse_permissions(config_values):
   permissions = config_values["make_permissions"]
   if permissions:
-    config_values["permissions"] = "".join([f"$(ADB) shell pm grant {config_values['make_app_debug_package']} {p};\\\n" for p in permissions.split(",")])
+    permissions_list = [f"$(ADB) shell pm grant {config_values['make_app_debug_package']} {p};\\\n" for p in permissions.split(",")]
+    config_values["permissions"] = "".join(permissions_list)
   else:
     config_values["permissions"] = ""
+
+def parse_carving_options(config_values):
+    filter_methods = config_values["make_carving_filter_methods"]
+    if filter_methods:
+        filter_method_list = [f"--filter-method {method} \\" for method in re.split("\s*,\s*", filter_methods)]
+        config_values["make_carving_filter_methods"] = "\n".join(filter_method_list)
+    else:
+        config_values["make_carving_filter_methods"] = "\\"
+
+def parse_instrumentation_options(config_values):
+    skip_classes = config_values["make_instr_skip_classes"]
+    if skip_classes:
+        classes_list = [f"--skip-class={clazz} \\" for clazz in re.split("\s*,\s*", skip_classes)]
+        config_values["make_instr_skip_classes"] = "\n".join(classes_list)
+    else:
+        config_values["make_instr_skip_classes"] = "\\"
+
+    filter_packages = config_values["make_instr_filter_packages"]
+    if filter_packages:
+        packages_list = [f"--filter-package={package} \\" for package in re.split("\s*,\s*", filter_packages)]
+        config_values["make_instr_filter_packages"] = "\n".join(packages_list)
+    else:
+        config_values["make_instr_filter_packages"] = "\\"
+
+    filter_classes = config_values["make_instr_filter_classes"]
+    if filter_classes:
+        classes_list = [f"--filter-class={clazz} \\" for clazz in re.split("\s*,\s*", filter_classes)]
+        config_values["make_instr_filter_classes"] = "\n".join(classes_list)
+    else:
+        config_values["make_instr_filter_classes"] = "\\"
 
 if len(sys.argv) == 1:
     print("Missing application root directory")
@@ -25,6 +57,8 @@ else:
         config.read(config_path)
         config_values = config["MakeConfiguration"]
         parse_permissions(config_values)
+        parse_carving_options(config_values)
+        parse_instrumentation_options(config_values)
         with open(template_path, "r") as template_file:
             template = Template(template_file.read())
             output = template.substitute(config_values)
